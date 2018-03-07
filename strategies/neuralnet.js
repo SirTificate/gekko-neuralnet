ce51749ee8d509415a495743fc053374810a3f3d
SirTificate <36823811+SirTificate@users.noreply.github.com>
2018-03-07 18:52:24 +0100

    cleanup + SMMA as input
    cleaned the code
    added comments
    switched to an SMMA as input for the net

--- strategies/neuralnet.js
+++ strategies/neuralnet.js
@@ -6,24 +6,40 @@ var log = require('../core/log.js');
 
 var config = require ('../core/util.js').getConfig();
 
+var SMMA = require('./indicators/SMMA.js');
 
 var strategy = {
-
+  // stores the candles
   priceBuffer : [],
   predictionCount : 0,
-  batchsize : 5,
+
+  batchsize : 1,
+  // no of neurons for the layer
   layer_neurons : 0,
+  // activaction function for the first layer, when neurons are > 0
   layer_activation : 'tanh',
+  // normalization factor
   scale : 1,
+  // stores the last action (buy or sell)
   prevAction : 'wait',
+  //stores the price of the last trade (buy/sell)
   prevPrice : 0,
+  // counts the number of triggered stoploss events
   stoplossCounter : 0,
+
+  // if you want the bot to hodl instead of selling during a small dip
+  // use the hodle_threshold. e.g. 0.95 means the bot won't sell
+  // when unless the price drops below a 5% threshold of the last buy price (this.privPrice)
+  hodle_threshold : 1,
 
   // init the strategy
   init : function() {
 
     this.name = 'Neural Network';
     this.requiredHistory = config.tradingAdvisor.historySize;
+
+    // smooth the input to reduce the noise of the incoming data
+    this.SMMA = new SMMA(5);
 
     let layers = [
       {type:'input', out_sx:1, out_sy:1, out_depth: 1},
@@ -45,7 +61,7 @@ var strategy = {
       threshold : this.settings.stoploss_threshold
     });
 
-
+    this.hodle_threshold = this.settings.hodle_threshold || 1;
   },
 
   learn : function () {
@@ -54,27 +70,29 @@ var strategy = {
       let current_price = [this.priceBuffer[i + 1]];
       let vol = new convnetjs.Vol(data);
       this.trainer.train(vol, current_price);
-      let predicted_values = this.nn.forward(vol);
-      let accuracymatch = predicted_values.w[0] === current_price;
-      this.nn.backward(accuracymatch);
+      this.nn.forward(vol);
       this.predictionCount++;
-
     }
   },
 
   setNormalizeFactor : function(candle) {
-    this.scale = Math.pow(10,Math.trunc(candle.high).toString().length+1);
+    this.scale = Math.pow(10,Math.trunc(candle.high).toString().length+2);
     log.debug('Set normalization factor to',this.scale);
   },
 
   update : function(candle)
   {
+    // play with the candle values to finetune this
+    this.SMMA.update( (candle.high + candle.close + candle.low + candle.vwp) /4);
+    let smmaFast = this.SMMA.result;
+
     if (1 === this.scale && 1 < candle.high && 0 === this.predictionCount) this.setNormalizeFactor(candle);
 
-    this.priceBuffer.push(candle.close / this.scale );
+    this.priceBuffer.push(smmaFast / this.scale );
     if (2 > this.priceBuffer.length) return;
 
-    for (i=0;i<3;++i) this.learn();
+     for (i=0;i<3;++i)
+      this.learn();
 
     while (this.settings.price_buffer_len < this.priceBuffer.length) this.priceBuffer.shift();
   },
@@ -84,7 +102,9 @@ var strategy = {
     if ('buy' === event.action) {
       this.indicators.stoploss.long(event.price);
     }
+    // store the previous action (buy/sell)
     this.prevAction = event.action;
+    // store the price of the previous trade
     this.prevPrice = event.price;
 
   },
@@ -113,6 +133,11 @@ var strategy = {
       let meanp = math.mean(prediction, currentPrice);
       let meanAlpha = (meanp - currentPrice) / currentPrice * 100;
 
+
+      // sell only if the price is higher than the buying price or if the price drops below the threshold
+      // a hodle_threshold of 1 will always sell when the NN predicts a drop of the price. play with it!
+      let signalSell = candle.close > this.prevPrice || candle.close < (this.prevPrice*this.hodle_threshold);
+
       let signal = meanp < currentPrice;
       if ('buy' !== this.prevAction && signal === false  && meanAlpha> this.settings.threshold_buy )
       {
@@ -121,7 +146,7 @@ var strategy = {
         return this.advice('long');
       }
       else if
-      ('sell' !== this.prevAction && signal === true && meanAlpha < this.settings.threshold_sell)
+      ('sell' !== this.prevAction && signal === true && meanAlpha < this.settings.threshold_sell && signalSell)
       {
 
         log.debug("Sell - Predicted variation: ",meanAlpha);
